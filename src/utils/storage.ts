8b523c1caeb79609b64e19202975b4405aa823fa
Juerg Hunziker <juerg.hunziker@liip.ch>
2024-04-12 10:16:44 +0200

    check if STORAGE_LOCAL_ROOT env variable is defined

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,13 @@
 import DriverLocal from '@directus/storage-driver-local';
+import { RokkaException } from "../exceptions";
 
 export function getStorage(): DriverLocal {
-	return new DriverLocal({ root: process.env.STORAGE_LOCAL_ROOT || '' });
+	if (!process.env.STORAGE_LOCAL_ROOT) {
+		throw new RokkaException(
+			`STORAGE_LOCAL_ROOT env variable not defined on server!`,
+			500,
+			'ENV_VARIABLE_MISSING'
+		);
+	}
+	return new DriverLocal({ root: process.env.STORAGE_LOCAL_ROOT });
 }
